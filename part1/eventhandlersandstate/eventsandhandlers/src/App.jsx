924696e374180a7a8b6479b4e4890464450e6e9d
Terrence-Beckham <terrencedbeckham@gmail.com>
2026-05-05 07:49:27 +0300

    Added unicafe app

--- part1/eventhandlersandstate/eventsandhandlers/src/App.jsx
+++ part1/eventhandlersandstate/eventsandhandlers/src/App.jsx
@@ -1,39 +1,79 @@
 import {useState} from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
+
+// const Display = ({counter}) => <div>{counter}</div>
 
 
-const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
+// const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+    return (
+        <div>
+            button press history: {props.allClicks.join(' ')}
+        </div>
+    )
+}
 
+const hello = (who)=> {
+const handler = () => {
+    console.log('hello',who)
+}
+return handler
+}
 
+const Button = (props) => {
+    console.log('props value is', props)
+    const {onClick, text} = props
+    return (
+        <button onClick={onClick}>
+            {text}
+        </button>
+    )
+}
 const App = () => {
-    const [counter, setCounter] = useState(0)
-    console.log('rendering with counter value', counter)
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
 
-    // setTimeout(() => setCounter(counter + 1), 1000)
-    const increaseByOne = () => {
-        console.log('increasing, value before', counter)
-        setCounter(counter + 1)
+
+    const handleLeftClick = () => {
+        console.log('left bottom clicked')
+        setAll(allClicks.concat('L'))
+        const updatedLeft = left + 1
+        console.log('left before', left)
+        setLeft(updatedLeft)
+        console.log('left after', updatedLeft)
+        setTotal(updatedLeft + right)
     }
 
-    const decreaseByOne = () => {
-        console.log('decreasing value before', counter)
-        setCounter(counter - 1)
-    }
-    const setToZero = () => {
-        console.log('resetting to zero, value before', counter)
-        setCounter(0)
-    }
 
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+        setTotal(left + right)
+    }
 
     return (
         <div>
-            <Display counter={counter}/>
-            <Button onClick={increaseByOne} text='plus'/>
-            <Button onClick={setToZero} text='zero'/>
-            <Button onClick={decreaseByOne} text='minus'/>
+            {left}
+            <Button onClick={handleLeftClick} text='left'/>
+            <Button onClick={handleRightClick} text='right'/>
+
+            {right}
+            <History allClicks={allClicks}/>
+            <button onClick={hello('react')}></button>
+            <p>total {total}</p>
+            <button onClick={()=>console.log('button pressed')} > Touch Me</button>
         </div>
     )
+
 }
 
 export default App
