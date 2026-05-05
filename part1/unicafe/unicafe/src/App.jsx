1ccc62dd9608c3131b39597f760479bffe23396e
Terrence-Beckham <terrencedbeckham@gmail.com>
2026-05-05 10:32:20 +0300

    Added Finished unicafe app

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,122 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
+const Button = (props) => {
+    const {onClick, text} = props
+    return (
+        <button onClick={onClick}>
+            {text}
         </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    )
 }
 
+const Statistics = (props) => {
+    const {good, neutral, bad} = props
+    const all = good + neutral + bad
+    const average = (good + (bad * -1)) / all
+    const positive = (good / all) * 100
+
+    const roundToHundreths = (number) => {
+        return Math.round(number * 100) / 100;
+    }
+    // const average = (clicks.good + (clicks.bad * -1)) / clicks.total
+    if (all === 0) {
+        return <p><b>No Feedback Given</b></p>
+    }
+
+    // return (
+    //     <div>
+    //         <p>Good {good}</p>
+    //         <p>Neutral {neutral}</p>
+    //         <p>Bad {bad}</p>
+    //         <p>All {all}</p>
+    //         <p>Average {average}</p>
+    //         <p>Positive {positive}</p>
+    //     </div>
+    // )
+    return (
+        <div>
+
+            <table>
+                <tbody>
+                <tr>
+                    <td>Good {good}</td>
+                </tr>
+                <tr>
+                    <td>Neutral {neutral}</td>
+                </tr>
+                <tr>
+                    <td>Bad {bad}</td>
+                </tr>
+                <tr>
+                    <td>All {all}</td>
+                </tr>
+                <tr>
+                    <td>Average {roundToHundreths(average)}</td>
+                </tr>
+                <tr>
+                    <td>Positive {roundToHundreths(positive)} %</td>
+                </tr>
+                </tbody>
+            </table>
+
+
+        </div>
+
+    )
+}
+
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodFeedback = () => {
+        const newGood = good + 1
+        setGood(newGood)
+    }
+    const handleNeutralFeedback = () => {
+        const newNeutral = neutral + 1
+        setNeutral(newNeutral)
+    }
+    const handleBadFeedback = () => {
+        const newBad = bad + 1
+        setBad(newBad)
+    }
+
+    return (
+        <div>
+            <h1>Give Feedback</h1>
+            <Button onClick={handleGoodFeedback} text='Good'> </Button>
+            <Button onClick={handleNeutralFeedback} text='Nuetral'> </Button>
+            <Button onClick={handleBadFeedback} text='Bad'> </Button>
+            <h1>Statistics</h1>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
+
+        </div>
+        // <Statistics good={good} neutral={neutral} bad={bad}/>
+    )
+
+
+}
+
+
 export default App
+// const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right + 1)
+//     setTotal(left + right)
+// }
+//
